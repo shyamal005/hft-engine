d5d50cb2d74c9c7ad0aa40bb63ca7fd566b01522
Shyamal Kudupudi <96627688+shyamal005@users.noreply.github.com>
2025-11-30 01:21:36 +0530

    Update OrderBook.java

--- src/main/java/com/hft/engine/OrderBook.java
+++ src/main/java/com/hft/engine/OrderBook.java
@@ -7,10 +7,9 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Component
 public class OrderBook {
-    // NavigableMap allows generic type matching; TreeMap keeps keys sorted
-    // Bids: Descending (Highest price first)
+    
     private final NavigableMap<Double, Double> bids = new TreeMap<>(Collections.reverseOrder());
-    // Asks: Ascending (Lowest price first)
+    
     private final NavigableMap<Double, Double> asks = new TreeMap<>();
     
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
@@ -29,14 +28,14 @@ public class OrderBook {
         }
     }
 
- // Inside getSnapshot()
+ 
 public JSONObject getSnapshot(long eventTime) {
     lock.readLock().lock();
     try {
         JSONObject json = new JSONObject();
         json.put("eventTime", eventTime); 
         
-        // NEW: Send the time right now (Server Output Time)
+        
         json.put("serverTime", System.currentTimeMillis()); 
         
         json.put("bids", getTopLevels(bids));
@@ -51,7 +50,7 @@ public JSONObject getSnapshot(long eventTime) {
         List<List<Double>> levels = new ArrayList<>();
         int count = 0;
         for (Map.Entry<Double, Double> entry : map.entrySet()) {
-            if (count++ >= 10) break; // Only send top 10 levels
+            if (count++ >= 10) break; 
             levels.add(List.of(entry.getKey(), entry.getValue()));
         }
         return levels;
