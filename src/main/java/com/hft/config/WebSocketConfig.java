ce772381b2ed87b438f32dbd2f487db481ff968d
Shyamal Kudupudi <96627688+shyamal005@users.noreply.github.com>
2025-11-30 01:20:34 +0530

    Update WebSocketConfig.java

--- src/main/java/com/hft/config/WebSocketConfig.java
+++ src/main/java/com/hft/config/WebSocketConfig.java
@@ -16,6 +16,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(uiBridge, "/stream")
-                .setAllowedOrigins("*"); // <--- THIS IS MANDATORY
+                .setAllowedOrigins("*"); 
     }
 }
